ede51e3e62db2ad460137708f92c41062b0dbed2
Aldry Albuquerque <albuquerquealdry>
2023-10-04 08:58:20 -0300

    add finops tags tf

---
+++ tags-models/tags.tf
@@ -0,0 +1,9 @@
+variable "tags" {
+  default = {
+    BU            = "AYO-001"
+    Environment   = "dev"
+    Name          = "serveless-resources"
+    Department    = "eng software"
+    Owner         = "kidboo squard"
+    Workload      = "booking-system"
+  }
